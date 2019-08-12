d85c408adc9505e27718ea25059c4c14b78fa3c2
bushj10 <bushj10@nationwide.com>
2019-08-12 12:20:52 -0400

    New changes (TESTING)

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello World!");
+        double pi, r, a;
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r * r;
+
+        System.out.println("Area of circle is  " + a);
+    }
+}
